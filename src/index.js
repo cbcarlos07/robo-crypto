22b5ba7b3b44576931a41a8967334c072b3870d5
Carlos Bruno <f515122@corp.caixa.gov.br>
2025-02-11 15:38:01 -0300

    RSI

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 const axios = require('axios')
+const {format} = require('date-fns')
 
 const SYMBOL = 'BTCUSDT'
 const PERIOD = 14
 
-const API_URL = 'https://testnet.binance.vision'; //https://api.binance.com
+const API_URL = 'https://api.binance.com' //'https://testnet.binance.vision'; //https://api.binance.com
 
 let isOpened = false
 
-const average = (prices, period, startIndex) => {
+const averages = (prices, period, startIndex = 1) => {
     let gains = 0
     let losses = 0
-
-    for (let index = 0; period && (index + startIndex) < prices.length; index++) {
-        const element = array[index];
+    //console.log('period', period);
+    //console.log('length', prices.length);
+    
+    for (let index = 0; index < period && (index + startIndex) < prices.length; index++) {
+        
         const diff = prices[ index + startIndex] - prices[ index + startIndex - 1 ]
+        
+        
         if( diff >= 0 ){
             gains += diff
         }else{
@@ -23,21 +28,54 @@ const average = (prices, period, startIndex) => {
 
     let avgGains = gains / period
     let avgLosses = losses / period
+    return {avgGains, avgLosses}
+}
+
+const RSI = (prices, period) => {
+    let avgGains = 0, avgLosses = 0
+    for (let index = 1; index < prices.length; index++) {
+        let newAvarages = averages( prices, period, index )
+        //console.log('newAvarages',newAvarages);
+        
+        if( index === 1 ){
+            avgGains = newAvarages.avgGains
+            avgLosses = newAvarages.avgLosses
+            continue
+        }
+
+        avgGains = (avgGains * (period - 1) + newAvarages.avgGains ) / period
+        avgLosses =  (avgLosses * (period - 1) + newAvarages.avgLosses ) / period
+        
+    }
+    
+    
+    const rs = avgGains / avgLosses
+    return 100 - ( 100 / (1 + rs ) )
 }
 
 const start = async () => {
 
     const { data } = await axios.get(`${API_URL}/api/v3/klines?limit=100&interval=15m&symbol=${SYMBOL}`)
     const candle = data[ data.length - 1 ]
-    const price = parseFloat( candle[4] ) 
+    const lastPrice = parseFloat( candle[4] ) 
     console.clear()
-    console.log("Price", price);
+    console.log(format(new Date(), "dd/MM/yyyy HH:mm:ss"));
     
-    if( price <= BUY_PRICE && !isOpened ){
-        console.warn('Comprar');
+    console.log("Price", lastPrice);
+    //console.log('data',data);
+    //process.exit()
+    const prices = data.map(k => parseFloat( k[ 4 ] ))
+    //console.log('prices', prices);
+    
+    const rsi = RSI(prices, PERIOD)
+    console.log('RSI', rsi);
+    
+    
+    if( rsi < 30 && !isOpened ){
+        console.log('Sobrevendido, hora de comprar');
         isOpened = true
-    }else if( price >= SELL_PRICE && isOpened ){
-        console.log('Vender');
+    }else if( rsi > 70 && isOpened ){
+        console.log('Sobrecomprado, hora de Vender');
         isOpened = false
     }else{
         console.log('Aguardar');
