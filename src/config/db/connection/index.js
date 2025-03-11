dcd85f168ac86f08159e8c0494734cb63575ffae
Carlos Bruno <cbcarlos08u@gmail.com>
2025-03-11 16:08:36 -0300

    url

--- src/config/db/connection/index.js
+++ src/config/db/connection/index.js
@@ -3,7 +3,7 @@ const { MONGO_USER, MONGO_PWD, MONGO_DB, MONGO_URL } = process.env
 const url = MONGO_URL.replace('user',MONGO_USER)
                      .replace('pwd', MONGO_PWD)
                      .replace('database',MONGO_DB)
-const MONGODB_URI = url//`mongodb://localhost:27017/${MONGO_DB}`
+const MONGODB_URI = `mongodb://localhost:27017/${MONGO_DB}`
 
 mongoose.set('strictQuery', false); // Preparar para a mudança futura
 
